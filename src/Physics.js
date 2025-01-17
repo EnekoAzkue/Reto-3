a85508700acf76b579bbe828830e58f2af4f9292
Eneko Azkue <eneko.azkue@ikasle.aeg,eus>
2025-01-17 09:39:24 +0100

    Clase de fisicas para el movimiento de los personajes

--- src/Physics.js
+++ src/Physics.js
@@ -1,9 +1,17 @@
 export default class Physics
 {
-    constructor(vLimit)
+    constructor(vLimit,aLimit,omega,angle,xRotCenter,yRotCenter)
     {
-        this.vx = 0;            //Velocida actual en el eje X(px/s)
-        this.vy = 0;            //Velocida actual en el eje Y(px/s)
-        this.vLimit = vLimit;   //
+        this.vx = 0;                        //Velocidad actual en el eje X(px/s)
+        this.vy = 0;                        //Velocidad actual en el eje Y(px/s)
+        this.vLimit = vLimit;               //Velocidad maxima a la que puede ir el sprite
+        this.ax = 0;                        //Aceleracion en eje X
+        this.ay = 0;                        //Aceleracion en eje Y
+        this.aLimit = aLimit;               //Aceleracion al limite. (Por defecto 0. No hay aceleracion)
+        this.omega = omega;                 //Velocidad angular rad/s
+        this.angle = angle;                 //Angulo actual(rad)
+        this.xRotCenter = xRotCenter;       //Centro de rotacion del sprite X(Movimiento circular)
+        this.yRotCenter = yRotCenter;       //Centro de rotacion del sprite Y(Movimineto circular)
+
     }
 }
