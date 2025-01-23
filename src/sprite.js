b2831407710e716afe7e395f5d5bf39fbc87b7db
Eneko Azkue <eneko.azkue@ikasle.aeg,eus>
2025-01-23 13:32:34 +0100

    añadir hitbox

--- src/sprite.js
+++ src/sprite.js
@@ -1,14 +1,20 @@
 //Clase gestora de los sprites
 export default class Sprite
 {
-    constructor(id, state, xPos, yPos, imageSet, frames, physics)
+    constructor(id, state, xPos, yPos, imageSet, frames, physics, hitBox)
     {
-        this.id         = id;       //Tipo de Sprite
-        this.state      = state;    //Estado de animacion del sprite
-        this.xPos       = xPos;     //Posicion en X en Canvas
-        this.yPos       = yPos;     //Posicion en Y en Canvas
-        this.imageSet   = imageSet; //Datos de las imagenes del sprite
-        this.frames     = frames;   //Datos de los frames de animacion
-        this.physics    = physics;  //Datos de las fisicas
+        this.id                                     = id;       //Tipo de Sprite
+        this.state                                  = state;    //Estado de animacion del sprite
+        this.xPos                                   = xPos;     //Posicion en X en Canvas
+        this.yPos                                   = yPos;     //Posicion en Y en Canvas
+        this.imageSet                               = imageSet; //Datos de las imagenes del sprite
+        this.frames                                 = frames;   //Datos de los frames de animacion
+        this.physics                                = physics;  //Datos de las fisicas
+        this.hitBox                                 = hitBox;
+        this.isCollidingWithPlayer                  = false; 
+        this.isCollidingWithObstacleOnTheTop        = false;
+        this.isCollidingWithObstacleOnTheLeft       = false;
+        this.isCollidingWithObstacleOnTheBottom     = false;
+        this.isCollidingWithObstacleOnTheRight      = false;
     }
 }
