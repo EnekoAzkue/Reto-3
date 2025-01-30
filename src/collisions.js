f8feb9fd4d5f5a5588e05d2c33867d784b4605b5
Eneko Azkue <eneko.azkue@ikasle.aeg,eus>
2025-01-30 13:39:11 +0100

    colision para la explosion

--- src/collisions.js
+++ src/collisions.js
@@ -1,7 +1,7 @@
 import globals from "./globals.js";
 import { Block, State } from "./constants.js";
 
-export default function detectCollisions() {
+export function detectCollisionsPlayer() {
     // Calculamos colisión del player con cada uno de los sprites
     for (let i = 1; i < globals.sprites.length; i++) {
         const sprite = globals.sprites[i];
@@ -14,6 +14,19 @@ export default function detectCollisions() {
     detectCollisionBetweenPlayerAndMapObstacles();  // Colisión con bloques
 }
 
+export function detectCollisionsExplosion() {
+    // Calculamos colisión de la explosion con cada uno de los sprites
+    for (let i = 0; i < globals.sprites.length; i++) {
+        const sprite = globals.sprites[i];
+        if (sprite.id != 1) 
+        {
+            detectCollisionBetweenExplosionAndSprite(sprite);  // Colisión con sprite
+        }
+    }
+
+    // Calculamos colisión de la explosion con los obstáculos del mapa
+    detectCollisionBetweenExplosionAndMapObstacles();  // Colisión con bloques
+}
 
 function detectCollisionBetweenPlayerAndSprite(sprite) {
     // Resetear el estado de colisión
@@ -119,7 +132,30 @@ function detectCollisionBetweenPlayerAndSprite(sprite) {
     }
 }
 
+function detectCollisionBetweenExplosionAndSprite(sprite) {
+    // Resetear el estado de colisión
+    sprite.isCollidingWithExplosion = false;
 
+    // Datos del jugador
+    const explosion = globals.sprites[1];
+    const x1 = explosion.xPos + explosion.hitBox.xOffset;
+    const y1 = explosion.yPos + explosion.hitBox.yOffset;
+    const w1 = explosion.hitBox.xSize;
+    const h1 = explosion.hitBox.ySize;
+
+    // Datos del sprite
+    const x2 = sprite.xPos + sprite.hitBox.xOffset;
+    const y2 = sprite.yPos + sprite.hitBox.yOffset;
+    const w2 = sprite.hitBox.xSize;
+    const h2 = sprite.hitBox.ySize;
+
+    const isOverlap = rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2);
+    if (isOverlap) {
+        // Si hay colisión, marcarla como tal
+        sprite.isCollidingWithExplosion = true;
+        //console.log("hay colision")
+    }
+}
 
 function rectIntersect(x1,y1,w1,h1,x2,y2,w2,h2)
 {
@@ -145,7 +181,7 @@ function getMapTileId(xPos,yPos)
 
     const fil = Math.floor(xPos/brickSize);
     const col = Math.floor(yPos/brickSize);
-
+    //console.log(`fil: ${fil} col: ${col}`)
 
     return levelData[col][fil];
 
@@ -240,25 +276,25 @@ function detectCollisionBetweenPlayerAndMapObstacles()
 
             break;
             case State.LEFT:
-    xPos = player.xPos + player.hitBox.xOffset;
-    yPos = player.yPos + player.hitBox.yOffset;
-    isCollidingOnPos1 = isCollidingWithObstacleAt(xPos, yPos, obstacleId3, obstacleId4, obstacleId5);
+            xPos = player.xPos + player.hitBox.xOffset;
+            yPos = player.yPos + player.hitBox.yOffset;
+            isCollidingOnPos1 = isCollidingWithObstacleAt(xPos, yPos, obstacleId3, obstacleId4, obstacleId5);
 
-    yPos = player.yPos + player.hitBox.yOffset + player.hitBox.ySize ;
-    isCollidingOnPos2 = isCollidingWithObstacleAt(xPos, yPos, obstacleId3, obstacleId4, obstacleId5);
+            yPos = player.yPos + player.hitBox.yOffset + player.hitBox.ySize ;
+            isCollidingOnPos2 = isCollidingWithObstacleAt(xPos, yPos, obstacleId3, obstacleId4, obstacleId5);
 
-    isColliding = isCollidingOnPos1 || isCollidingOnPos2;
+            isColliding = isCollidingOnPos1 || isCollidingOnPos2;
 
-    if (isColliding) {
-        player.isCollidingWithObstacleOnTheLeft = true;
+            if (isColliding) {
+                player.isCollidingWithObstacleOnTheLeft = true;
 
-        // Usar cálculo de solapamiento ajustado
-        overlap = brickSize - (xPos % brickSize);
-        if (overlap === brickSize) overlap = 0;
+                // Usar cálculo de solapamiento ajustado
+                overlap = brickSize - (xPos % brickSize);
+                if (overlap === brickSize) overlap = 0;
 
-        player.xPos += overlap; // Ajuste hacia la derecha
-    }
-    break;
+                player.xPos += overlap; // Ajuste hacia la derecha
+            }
+            break;
 
 
             case State.DOWN:
@@ -291,30 +327,167 @@ function detectCollisionBetweenPlayerAndMapObstacles()
             break;
 
             case State.UP:
-    // Calcular la posición para la primera colisión
-    xPos = player.xPos + player.hitBox.xOffset;
-    yPos = player.yPos + player.hitBox.yOffset;
-    isCollidingOnPos1 = isCollidingWithObstacleAt(xPos, yPos, obstacleId1, obstacleId2, obstacleId4, obstacleId5);
+            // Calcular la posición para la primera colisión
+            xPos = player.xPos + player.hitBox.xOffset;
+            yPos = player.yPos + player.hitBox.yOffset;
+            isCollidingOnPos1 = isCollidingWithObstacleAt(xPos, yPos, obstacleId1, obstacleId2, obstacleId4, obstacleId5);
 
-    // Calcular la posición para la segunda colisión
-    xPos = player.xPos + player.hitBox.xOffset + player.hitBox.xSize - 5; // Asegurarse de usar un desplazamiento consistente
-    isCollidingOnPos2 = isCollidingWithObstacleAt(xPos, yPos, obstacleId1, obstacleId2, obstacleId4, obstacleId5);
+            // Calcular la posición para la segunda colisión
+            xPos = player.xPos + player.hitBox.xOffset + player.hitBox.xSize - 5; // Asegurarse de usar un desplazamiento consistente
+            isCollidingOnPos2 = isCollidingWithObstacleAt(xPos, yPos, obstacleId1, obstacleId2, obstacleId4, obstacleId5);
 
-    // Verificar si hay colisión
-    isColliding = isCollidingOnPos1 || isCollidingOnPos2;
+            // Verificar si hay colisión
+            isColliding = isCollidingOnPos1 || isCollidingOnPos2;
 
-    if (isColliding) {
-        player.isCollidingWithObstacleOnTheTop = true;
+            if (isColliding) {
+                player.isCollidingWithObstacleOnTheTop = true;
 
-        // Usar cálculo ajustado para el solapamiento
-        overlap = brickSize - (yPos % brickSize);
-        if (overlap === brickSize) overlap = 0;
+                // Usar cálculo ajustado para el solapamiento
+                overlap = brickSize - (yPos % brickSize);
+                if (overlap === brickSize) overlap = 0;
 
-        // Ajustar la posición del jugador hacia abajo para corregir el solapamiento
-        player.yPos += overlap;
+                // Ajustar la posición del jugador hacia abajo para corregir el solapamiento
+                player.yPos += overlap;
+            }
+            break;
+
     }
-    break;
 
-    }
+}
 
+function detectCollisionBetweenExplosionAndMapObstacles()
+{
+
+    const explosion = globals.sprites[1];
+    
+    //Reset collision state 
+    explosion.isCollidingWithObstacleOnTheRight = false;
+
+
+    //Variables to use
+    let xPos;
+    let yPos;
+    let isCollidingOnPos1;
+    let isCollidingOnPos2;
+    let isCollidingOnPos3;
+    let isCollidingOnPos4;
+    let isCollidingOnPos5;
+    let isCollidingOnPos6;
+    let isCollidingOnPos7;
+    let isCollidingOnPos8;
+
+    const brickSize = globals.Level.imageSet.gridSize;
+
+    //ID del obstaculo
+    //obstaculos:
+    //3,5,9,10,11,12,22,23,27,28
+    const obstacleId1 = Block.MAZE_TOP_WALL_1;
+    const obstacleId2 = Block.MAZE_TOP_WALL_2;
+    const obstacleId3= Block.MAZE_LEFT_WALL_3;
+    const obstacleId4 = Block.MAZE_BLOCK_1;
+    const obstacleId5 = Block.MAZE_BLOCK_2;
+    const obstacleId6 = Block.MAZE_RIGHT_WALL_2;
+    const obstacleId7 = Block.MAZE_BOT_WALL_1;
+    const obstacleId8 = Block.MAZE_BOT_WALL_2;
+    const obstacleId9 = Block.MAZE_BOT_WALL_3;
+    const obstacleId10 = Block.MAZE_BOT_WALL_4;
+
+    //Primera colision en 
+    //(
+    //[X][-][-]
+    //[-][-][-]
+    //[-][-][-]
+    //)
+    xPos = explosion.xPos - 8;
+    yPos = explosion.yPos - 8;
+    isCollidingOnPos1 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+    //Segunda colision en 
+    //(
+    //[-][X][-]
+    //[-][-][-]
+    //[-][-][-]
+    //)
+    xPos = explosion.xPos + 8;
+    yPos = explosion.yPos - 8;    
+    isCollidingOnPos2 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+
+    //Tercera colision en 
+    //(
+    //[-][-][X]
+    //[-][-][-]
+    //[-][-][-]
+    //)
+    xPos = explosion.xPos + 24;
+    yPos = explosion.yPos - 8;
+    isCollidingOnPos3 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+
+    //Cuarta colision en 
+    //(
+    //[-][-][-]
+    //[X][-][-]
+    //[-][-][-]
+    //)
+    xPos = explosion.xPos - 8;
+    yPos = explosion.yPos + 8;    
+    isCollidingOnPos4 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+    
+    //Quinta colision en 
+    //(
+    //[-][-][-]
+    //[-][-][X]
+    //[-][-][-]
+    //)
+    xPos = explosion.xPos + 24;
+    yPos = explosion.yPos +  8;
+    isCollidingOnPos5 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+
+    //Sexta colision en 
+    //(
+    //[-][-][-]
+    //[-][-][-]
+    //[X][-][-]
+    //)
+    xPos = explosion.xPos - 8;
+    yPos = explosion.yPos + 24;    
+    isCollidingOnPos6 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+    
+    //Septima colision en 
+    //(
+    //[-][-][-]
+    //[-][-][-]
+    //[-][X][-]
+    //)
+    xPos = explosion.xPos + 8;
+    yPos = explosion.yPos + 24;
+    isCollidingOnPos7 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+
+
+    //Octava colision en 
+    //(
+    //[-][-][-]
+    //[-][-][-]
+    //[-][-][X]
+    //)
+    xPos = explosion.xPos + 24;
+    yPos = explosion.yPos + 24;    
+    isCollidingOnPos8 = isCollidingWithObstacleAt(xPos,yPos,obstacleId1,obstacleId2,obstacleId3,obstacleId4,obstacleId5,obstacleId6,obstacleId7,obstacleId8,obstacleId9,obstacleId10);
+    //console.log("Pos1: " + isCollidingOnPos1);
+    //console.log("Pos2: " + isCollidingOnPos2);
+    //console.log("Pos3: " + isCollidingOnPos3);
+    //console.log("Pos4: " + isCollidingOnPos4);
+    //console.log("Pos5: " + isCollidingOnPos5);
+    //console.log("Pos6: " + isCollidingOnPos6);
+    //console.log("Pos7: " + isCollidingOnPos7);
+    //console.log("Pos8: " + isCollidingOnPos8);
+
+    // if(isCollidingOnPos1)
+    // {
+    //     //Existe colision a la derecha
+    //     player.isCollidingWithObstacleOnTheRight = true;
+
+    //     //AJUSTE: Calculamos solapamiento(ovelap) y lo eliminamos 
+    //     //moviendo el personaje tantos pixeles como overlap a la izq
+    //     overlap = Math.floor(xPos) % brickSize;
+    //     player.xPos -= overlap;
+    // }
 }
